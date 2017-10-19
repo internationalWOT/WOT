4f4085778ab879172d798ef487d8b6f6c6293a4d
internationalWOT <oscar.nilsson1991@gmail.com>
2017-10-19 13:27:05 +0200

    ff

--- WindowsFormsApp1/Logik/ImportRSS.cs
+++ WindowsFormsApp1/Logik/ImportRSS.cs
@@ -12,4 +12,19 @@ namespace Logik
 
     }
 
-    
+    var reader = XmlReader.Create("http://api.sr.se/api/rss/program/4912");
+    var feed = SyndicationFeed.Load(reader);
+
+    string s = "";
+        foreach (SyndicationItem i in feed.Items)
+        {
+            s += i.Title.Text + "<br />" + i.Summary.Text + "<br />" + i.PublishDate.ToString() + "<br />";
+            foreach (SyndicationElementExtension extension in i.ElementExtensions)
+            {
+                XElement ele = extension.GetObject<XElement>();
+    s += ele.Name + " :: " + ele.Value + "<br />";
+            }
+s += "<hr />";
+        }
+        return s;
+}
